c83b2382f0e6874c2538b6f0a4cc34ca271b97c7
emi <lucasemilianomanganiello@gmail.com>
2024-08-05 15:14:28 -0300

    agregando login por front end

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const express = require('express');
+const path = require('path'); // Importa el módulo path
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
@@ -7,16 +7,28 @@ const config = require('./config/config');
 
 const app = express();
 
+// Configura el directorio de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define una ruta para servir el archivo login.html
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Define una ruta para servir el archivo protected.html
+app.get('/protected', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'protected.html'));
+});
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/', protectedRoutes);
 
-
-
 mongoose.connect(config.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error de conexión a MongoDB:', err));
+
 app.listen(config.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${config.PORT}`);
 });
